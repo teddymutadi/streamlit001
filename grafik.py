0aa15d63d4d7ced2787e853da1d8d88bf05459c6
Teddy Mutadi <81450446+teddymutadi@users.noreply.github.com>
2025-08-20 12:30:11 +0700

    Update grafik.py

--- grafik.py
+++ grafik.py
@@ -11,13 +11,15 @@ y = [2, 5, 3, 6, 8]
 # Buat grafik matplotlib
 fig, ax = plt.subplots()
 ax.plot(x, y, marker="o")
-ax.set_title("Contoh Grafik Garis")
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
+
+ax.set_title("Grafik Garis")
+ax.set_xlabel("Pupuk")
+ax.set_ylabel("Hasil Panen")
 
 # Tampilkan di Streamlit
 st.pyplot(fig)
 
 st.success("✅ Grafik Matplotlib berhasil ditampilkan di Streamlit!")
+
 
 
